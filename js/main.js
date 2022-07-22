41b54e97594da40309667b51d06eb98fa0fa3715
baly <kevin@baldev.it>
2022-07-22 17:16:12 +0200

    Project done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Milestone 1
 const posts = [
     {
         "id": 1,
@@ -55,3 +56,82 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const postContainer = document.querySelector('.posts-list')
+
+// milestone 2
+posts.forEach((e, i) => {
+
+    // post creation
+    let post = document.createElement('div')
+    post.classList.add('post')
+
+    // post header
+    let postHeader = document.createElement('div')
+    post.classList.add('post__header')
+
+    postHeader.innerHTML = `<div class="post-meta">                    
+                                <div class="post-meta__icon">
+                                    <img class="profile-pic" src="${e.author.image}" alt="Phil Mangione">                    
+                                </div>
+                                <div class="post-meta__data">
+                                    <div class="post-meta__author">${e.author.name}</div>
+                                    <div class="post-meta__time">4 mesi fa</div>
+                                </div>                    
+                            </div>`
+
+    // post body
+    let postBody = document.createElement('div')
+
+    postBody.innerHTML = `<div class="post__text">${e.content}</div>
+                            <div class="post__image">
+                                <img src="${e.media}" alt="">
+                            </div>
+    `
+
+    // post footer
+    let postFooter = document.createElement('div')
+    postFooter.classList.add('post_footer')
+
+    postFooter.innerHTML =`<div class="likes js-likes">
+                                <div class="likes__cta">
+                                    <a class="like-button  js-like-button" href="#" data-postid="${i}">
+                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                        <span class="like-button__label">Mi Piace</span>
+                                    </a>
+                                </div>
+                                <div class="likes__counter">
+                                    Piace a <b id="like-counter-${i}" class="js-likes-counter">80</b> persone
+                                </div>
+                            </div>
+    `
+
+    // append elemnts to DOM
+    postContainer.append(post)
+    post.append(postHeader, postBody, postFooter)
+
+    
+})
+
+
+// milestone 2
+const btnLike = document.querySelectorAll('.like-button')
+const likeCounter = document.querySelectorAll('.js-likes-counter')
+
+btnLike.forEach((e, i) => {
+    let clickCheck = false
+
+    e.addEventListener('click', (event) => {
+
+        event.preventDefault()
+
+        let likeValue = parseInt(likeCounter[i].innerHTML)
+        if (clickCheck == false){
+            likeValue++
+            clickCheck = true
+        }
+
+        likeCounter[i].innerHTML = likeValue
+        e.classList.add('like-button--liked')
+    })
+})
